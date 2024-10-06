1c2cf9e5feca1977ac0fa48669ebbaa34b532b4a
AntonelaDiPinto <dipintoantonela@gmail.com>
2024-10-06 01:04:38 -0300

    Se modifica el ejercicio pokeapi de la Clase 2 del Segundo Cuatrimestre

--- Clase2SC/pokeapi.js
+++ Clase2SC/pokeapi.js
@@ -1,4 +1,4 @@
-let contenedor = document.getElementById("contenedor");
+/*let contenedor = document.getElementById("contenedor");
 
 fetch("https://pokeapi.co/api/v2")
     .then(resp => resp.json())
@@ -23,4 +23,39 @@ fetch("https://pokeapi.co/api/v2")
 
         })
     )
+    .catch(err => console.log(err))*/
+
+let contenedor = document.getElementById("contenedor");
+
+async function pokemonDetails(url) {
+    const response = await fetch(url);
+    return response.json();
+}
+fetch("https://pokeapi.co/api/v2/pokemon?limit=36")
+    .then(resp => resp.json())
+    .then(data =>{
+        console.log(data)
+        const details = data.results.map(pokemon => pokemonDetails(pokemon.url));
+        return Promise.all(details);
+    })
+       .then(pokemons => {
+        pokemons.forEach(pokemon => {
+            contenedor.innerHTML += ` <div class="card">
+                                            <div class="contenido">
+                                            <div class="pokemon-imagen">
+                                            <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                                            </div>
+                                            <h2> ${pokemon.name} </h2>
+                                            <p class="pokemon-id">#${pokemon.id}</p>
+                                            </div>
+                                            <div class="pokemon-tipos">
+                                            ${pokemon.order}
+                                            </div>
+                                            <div class="pokemon-stats">
+                                                    <p class="stat">${pokemon.height}m</p>
+                                                    <p class="stat">${pokemon.weight}kg</p>
+                                            </div>
+                                            </div>`
+        });
+    })
     .catch(err => console.log(err))
