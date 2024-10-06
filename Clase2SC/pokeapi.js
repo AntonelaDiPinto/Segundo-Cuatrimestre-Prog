9347c82a810f0f5c0cfa2dc379d570fb0103bc09
AntonelaDiPinto <dipintoantonela@gmail.com>
2024-10-06 01:07:43 -0300

    Se modifica el archivo pokeapi de la Clase 2 del Segundo Cuatrimestre

--- Clase2SC/pokeapi.js
+++ Clase2SC/pokeapi.js
@@ -1,29 +1,3 @@
-/*let contenedor = document.getElementById("contenedor");
-
-fetch("https://pokeapi.co/api/v2")
-    .then(resp => resp.json())
-    .then(data =>
-        data.results.forEach(pokemon => {
-            contenedor.innerHTML += ` <div class="card">
-                                            <div class="contenido">
-                                            <div class="pokemon-imagen">
-                                            <img src="${pokemon.sprites.other["official-artwork"].front_default}" alt="${pokemon.name}">
-                                            </div>
-                                            <h2> ${pokemon.name} </h2>
-                                            <p class="pokemon-id">#${pokemon.Id}</p>
-                                            </div>
-                                            <div class="pokemon-tipos">
-                                            ${tipos}
-                                            </div>
-                                            <div class="pokemon-stats">
-                                                    <p class="stat">${pokemon.height}m</p>
-                                                    <p class="stat">${pokemon.weight}kg</p>
-                                            </div>
-                                            </div>`
-
-        })
-    )
-    .catch(err => console.log(err))*/
 
 let contenedor = document.getElementById("contenedor");
 
